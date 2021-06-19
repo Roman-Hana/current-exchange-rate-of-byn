80a85eb1206b18c5187a7296cee961cd354c2db5
Roman-Hana <romanhanaua@gmail.com>
2021-06-19 19:34:28 +0300

    working app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+const CurenncyAPI = "https://www.nbrb.by/api/exrates/rates?periodicity=0";
 
 function App() {
+  const [CurrencyOptions, setCurrencyOptions] = useState([]);
+
+  useEffect(() => {
+    fetch(CurenncyAPI)
+      .then((res) => res.json())
+      .then((data) => setCurrencyOptions(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>
+        Web page with the current exchange rate of the Belarusian ruble in
+        relation to all the foreign currencies established by the National Bank
+        of the Republic of Belarus.
+      </h1>
+      <table className="currencyTable">
+        <thead>
+          <tr>
+            <th>Currency Name</th>
+            <th>Number of units of foreign currency</th>
+            <th>Letter code of the currency</th>
+            <th>Official rate</th>
+          </tr>
+        </thead>
+        <tbody>
+          {CurrencyOptions.map((el) => (
+            <tr key={el.Cur_ID}>
+              <td>{el.Cur_Name}</td>
+              <td>{el.Cur_Scale}</td>
+              <td>{el.Cur_Abbreviation}</td>
+              <td>{el.Cur_OfficialRate}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
